5e6e9c5e22c7a6caca5b7c8c2d22408e0fd089c2
Aziza <rakhmonkulova.aziza@gmail.com>
2023-09-04 20:28:33 +0500

    upd

---
+++ exc.py
@@ -0,0 +1,7 @@
+# Given an independent variable x, find the value of a function y = 3x^6 − 6x^2 − 7. 
+
+x = 5
+
+print(3*x**6-6*x**2-7)
+
+
--- run.py
+++ run.py
@@ -1,18 +1,38 @@
 import random
+from PIL import Image
 import streamlit as s
 
-s.title('Dice Roller ')
+# Set up the Streamlit app
+s.title('Dice Roller')
 
-s.markdown('# This is Marcus')
 
+# Create a button to roll the dice
 roll_button = s.button('Roll the Dice')
 
+# Define a dictionary to map values to dice face image file names
+dice_faces = {
+    1: 'dice1.png',
+    2: 'dice2.png',
+    3: 'dice3.png',
+    4: 'dice4.png',
+    5: 'dice5.png',
+    6: 'dice6.png'
+}
 
+# Initialize an image variable
+dice_image = None
 
+# Check if the button is clicked
 if roll_button:
-    value = random.randint(1,6)
-    text = 'Dice value is ' + str(value)
-    # s.write(text)
+    value = random.randint(1, 6)
+    text = '# ' + str(value)
+    s.markdown(text)
+    
+    # Load and display the corresponding dice face image
+    image_path = './img/' + dice_faces.get(value, 'dice_unknown.png')  # Use a default image for unknown values
 
-    s.markdown(f"# {value} :game_die:")
+    dice_image = Image.open(image_path) # .resize((100,10))
+    s.image(dice_image, width=200)
+
+
 
