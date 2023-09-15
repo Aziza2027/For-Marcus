683d6a727f77c2a64b15ad05f6f3ae2778971239
Aziza <rakhmonkulova.aziza@gmail.com>
2023-09-15 20:20:57 +0500

    upd

--- exc.py
+++ exc.py
@@ -1,7 +1,20 @@
 # Given an independent variable x, find the value of a function y = 3x^6 − 6x^2 − 7. 
 
-x = 5
+# temp = int(input('Pizza temp: '))
 
-print(3*x**6-6*x**2-7)
+# if temp==100:
+#     print("Yes") 
+# else:
+#     print('NO')
+
+
+p1 = int(input('Password 1: '))
+p2 = int(input('Password 2: '))
+p3 = int(input('Password 3: '))
+
+if (p1 > 1 and p1 < 100) and (p2 > 1 and p2 < 100) and (p3 > 1 and p3 < 100):
+    print('YEAH')
+else:
+    print('NO')
 
 
